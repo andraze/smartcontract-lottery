5a74b3aeb0da7467b6ef62e984baa8c718105b8e
Andraž Erker <47939147+andraze@users.noreply.github.com>
2022-03-19 14:00:15 +0100

    Working...

---
+++ README.md
@@ -0,0 +1,9 @@
+1. Users can enter the lottery with ETH on a USD fee.
+2. Admin will choose when the lottery is over.
+3. Lottery will select a random winner.
+
+How do we want to test this?
+
+1. `mainnet-fork`
+2. `development` with mocks
+3. `testnet`
---
+++ scripts/helpful_scripts.py
@@ -0,0 +1,70 @@
+from brownie import (
+    network,
+    accounts,
+    config,
+    MockV3Aggregator,
+    Contract,
+    VRFCoordinatorMock,
+    LinkToken,
+)
+from web3 import Web3
+
+LOCAL_FORKED_ENVIORMENTS = ["mainnet-fork", "mainnet-fork-dev"]
+LOCAL_DEPLOYMENT_ENVIORNMENTS = ["development", "ganache-local"]
+
+
+def get_account(index=None, id=None):
+    if index:
+        return accounts[index]
+    if id:
+        return accounts.load(id)
+    if (
+        network.show_active() in LOCAL_DEPLOYMENT_ENVIORNMENTS
+        or network.show_active() in LOCAL_FORKED_ENVIORMENTS
+    ):
+        return accounts[0]
+    return accounts.add(config["wallets"]["from_key"])
+
+
+DECIMALS = 8
+STARTING_PRICE = 200_000_000_000
+
+
+def deploy_mocks(decimals=DECIMALS, starting_price=STARTING_PRICE):
+    account = get_account()
+    MockV3Aggregator.deploy(decimals, starting_price, {"from": get_account()})
+    link_token = LinkToken.deploy({"from": account})
+    VRFCoordinatorMock.deploy(link_token.address, {"from": account})
+    print("Mocks deployed!")
+
+
+contract_to_mock = {
+    "eth_usd_price_feed": MockV3Aggregator,
+    "vrf_coordinator": VRFCoordinatorMock,
+    "link_token": LinkToken,
+}
+
+
+def get_contract(contract_name):
+    """
+    This function will grab the cotract addresses from the brownie config if defined, otherwise it will deploy a mock version of that contract and return that mock contract.
+
+        Args:
+            contract_name (string) (E.g. 'eth_usd_price_feed')
+
+        Returns:
+            brownie.network.contract.ProjectContract: The most recently deployed version of this contract. (E.g. 'MockV3Aggregator')
+    """
+    contract_type = contract_to_mock[contract_name]  # E.g. MockV3Aggregator
+    if network.show_active() in LOCAL_DEPLOYMENT_ENVIORNMENTS:
+        if len(contract_type) <= 0:
+            # MockV3Aggregator.length
+            deploy_mocks()
+        contract = contract_type[-1]
+        # MockV3Aggregator[-1]
+    else:
+        contract_address = config["networks"][network.show_active()].get(contract_name)
+        contract = Contract.from_abi(
+            contract_type._name, contract_address, contract_type.abi
+        )
+    return contract
